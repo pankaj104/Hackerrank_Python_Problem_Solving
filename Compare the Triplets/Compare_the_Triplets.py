6a77e157a10199a3eadd30be5eb35b11acd7dd8f
pankaj104 <82790153+pankaj104@users.noreply.github.com>
2021-08-20 03:01:51 -0500

    Solved

--- Compare the Triplets/Compare_the_Triplets.py
+++ Compare the Triplets/Compare_the_Triplets.py
@@ -1,1 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+
+def compareTriplets(a, b):
+    u=0
+    v=0
+    for i in range(2):
+        if a[i]>b[i]:
+            u=u+1
+        else:
+            v=v+1
+    
+            
+    print (list(u,v))
+
+            
+            
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
 
