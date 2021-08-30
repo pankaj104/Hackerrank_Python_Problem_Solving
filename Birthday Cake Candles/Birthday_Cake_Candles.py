ad42ff87099edfd016e95df8c520a66d06c23f6b
pankaj104 <82790153+pankaj104@users.noreply.github.com>
2021-08-30 06:14:43 -0500

    Update Birthday_Cake_Candles.py

--- Birthday Cake Candles/Birthday_Cake_Candles.py
+++ Birthday Cake Candles/Birthday_Cake_Candles.py
@@ -1,1 +1,16 @@
 
+candles_count = int(input().strip())
+n=0
+candles = list(map(int, input().rstrip().split()))
+
+
+maxlength= max(candles)
+length= len(candles)
+
+for i in range(0,length) :
+    if(maxlength==candles[i]):
+        n=n+1
+       
+    else:
+        continue
+print (n)
