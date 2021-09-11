c8cfd929b3a48ee28a8f2700f996ee3782a33de4
pankaj104 <82790153+pankaj104@users.noreply.github.com>
2021-09-10 23:20:36 -0500

    Solved

--- Between Two Sets/Between_Two_Sets.py
+++ Between Two Sets/Between_Two_Sets.py
@@ -1,1 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def getTotalX(a, b):
+    maxA = max(a)
+    minB = min(b)
+    count = 0
+    for num in range(maxA, minB + 1):
+        left = all([num % numA == 0 for numA in a])
+        right = all([numB % num == 0 for numB in b])
+        count += left * right
+    return count
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
 
