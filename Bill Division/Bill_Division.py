5409d5b09e584de7dc59247435d839d0e5634435
pankaj104 <82790153+pankaj104@users.noreply.github.com>
2021-09-25 06:17:33 -0500

    Solved

--- Bill Division/Bill_Division.py
+++ Bill Division/Bill_Division.py
@@ -1,1 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    sum1=0
+      
+    for i in range(0,n):
+        if( i!=k):
+            sum1= sum1+bill[i]
+    
+     
+    bactual= int(sum1/2  )     
+    if (b> bactual):
+        print (b-bactual)
+    else:
+        print ("Bon Appetit")
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
 
